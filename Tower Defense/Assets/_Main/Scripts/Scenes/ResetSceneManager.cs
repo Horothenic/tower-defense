0c61fd86c4e435a3f3e4a9b5b5526fcd2cb80358
Ivan Herrera <ivan.herrera.m@outlook.com>
2021-01-16 17:14:36 -0600

    Base receives hits and resets scene when health depletes (#29)

---
+++ Tower Defense/Assets/_Main/Scripts/Scenes/ResetSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TowerDefense.Scenes
+{
+    public class ResetSceneManager : MonoBehaviour
+    {
+        #region FIELDS
+
+        [Header("CONFIGURATION")]
+        [SerializeField] private string sceneName = "Main";
+
+        #endregion
+
+        #region BEHAVIORS
+
+        public void ResetScene()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        #endregion
+    }
+}
