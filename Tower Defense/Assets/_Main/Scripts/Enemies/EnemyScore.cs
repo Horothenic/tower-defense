c2a475c5225a081304f1c6c35590492f0db0c93a
Ivan Herrera <ivan.herrera.m@outlook.com>
2021-01-16 16:54:59 -0600

    Enemies destroyed by health depletion give score

---
+++ Tower Defense/Assets/_Main/Scripts/Enemies/EnemyScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using Zenject;
+
+using TowerDefense.Scoring;
+
+namespace TowerDefense.Enemies
+{
+    public class EnemyScore : MonoBehaviour
+    {
+        #region FIELDS
+
+        [Inject] private ScoreManager scoreManager = null;
+
+        [Header("CONFIGURATIONS")]
+        [SerializeField] private int score = 10;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        public void GiveScore()
+        {
+            scoreManager.IncreaseScore(score);
+        }
+
+        #endregion
+    }
+}
