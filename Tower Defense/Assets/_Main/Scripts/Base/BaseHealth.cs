4c1baa4f7172ea00e8e264dfc1aa1b5d68f6265c
Ivan Herrera <ivan.herrera.m@outlook.com>
2021-01-16 17:12:48 -0600

    Base receives hits and resets scene when health depletes

---
+++ Tower Defense/Assets/_Main/Scripts/Base/BaseHealth.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+using Utilities.Inspector;
+
+namespace TowerDefense.Base
+{
+    public class BaseHealth : MonoBehaviour
+    {
+        #region FIELDS
+
+        [Header("CONFIGURATION")]
+        [SerializeField] private string enemyTag = "Enemy";
+        [SerializeField] private float health = 50;
+
+        [Header("STATES")]
+        [ReadOnly] [SerializeField] private float currentHealth = 0;
+
+        #endregion
+
+        #region EVENTS
+
+        [Header("EVENTS")]
+        public UnityEvent onHealthDepleted = null;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Awake()
+        {
+            currentHealth = health;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.CompareTag(enemyTag))
+                return;
+
+            ReceiveHit();
+        }
+
+        public void ReceiveHit()
+        {
+            if (--currentHealth <= 0)
+                onHealthDepleted?.Invoke();
+        }
+
+        #endregion
+    }
+}
