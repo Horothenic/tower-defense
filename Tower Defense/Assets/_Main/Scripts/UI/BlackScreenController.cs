f4276620cf4d8c68616c1a66dc1966ff9bbe2785
Ivan Herrera <ivan.herrera.m@outlook.com>
2021-01-17 21:58:25 -0600

    Added black transition on start and end

---
+++ Tower Defense/Assets/_Main/Scripts/UI/BlackScreenController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+using DG.Tweening;
+
+namespace TowerDefense.Scenes
+{
+    public class BlackScreenController : MonoBehaviour
+    {
+        #region FIELDS
+
+        [Header("COMPONENTS")]
+        [SerializeField] private CanvasGroup canvasGroup = null;
+
+        [Header("CONFIGURATIONS")]
+        [SerializeField] private float duration = 0.3f;
+        [SerializeField] private bool disappearOnStart = true;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Start()
+        {
+            if (disappearOnStart)
+                Disappear();
+        }
+
+        public void Appear(TweenCallback onComplete = null)
+        {
+            canvasGroup.DOFade(1, duration).From(0).OnComplete(onComplete);
+        }
+
+        public void Disappear(TweenCallback onComplete = null)
+        {
+            canvasGroup.DOFade(0, duration).From(1).OnComplete(onComplete);
+        }
+
+        #endregion
+    }
+}
